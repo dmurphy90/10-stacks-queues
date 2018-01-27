a21faf4ecaae914da0b28dcfc8bbcf1d8f2b3647
Dean <dean.murphy90@gmail.com>
2018-01-26 22:33:20 -0800

    still working on tests, need documentation too

--- lab-dean/lib/queue.js
+++ lab-dean/lib/queue.js
@@ -4,32 +4,38 @@ const Node = require('./node.js');
 
 module.exports = class {
   constructor() {
-    this.top = null,
-    this.bottom = null,
-    this.newestIndex = 1,
-    this.oldestIndex = 1,
+    this.front = null;
     this.size = 0;
   }
 
-  enqueue(val) {
-    let node = new Node(val);
-    
-    if(this.size === 0) this.top = node;
-    this.bottom = node;
-    this.newestIndex++;
+  enqueue(val){
+
+    let nd = new Node(val);
+
+    if(!this.front) {
+      this.front = nd;
+      this.size++;
+
+      return this;
+    }
+
+    for(var itr = this.front; itr.next; itr = itr.next);
+    itr.next = nd;
     this.size++;
-    
-    return this.bottom;
+
+    return this;
   }
 
-  dequeue() {
-    let temp = this.next;
-    
-    delete this.top;
-    this.top = temp;
-    this.oldestIndex++;
-    this.size--;
-    
-    return temp;
+  dequeue(){
+   
+    if(!this.front) throw new Error('No entries to remove.');
+   
+    let temp = this.front;
+   
+    this.front = temp.next;
+    temp.next = null;
+    this.size --;
+   
+    return this.front;
   }
 };
